af8befdc19db8df8dfef28f1c2a6b43f254d35cc
Simon Pascal Baur <sbausis@users.noreply.github.com>
2018-08-31 17:15:05 +0200

    function OctoPrintInstance_install / function OctoPrintInstance_configure

--- octoprintinstance.sh
+++ octoprintinstance.sh
@@ -42,14 +42,62 @@ function OctoPrintInstance_createUser() {
 
 }
 
-function OctoPrintInstance_delete() {
+function OctoPrintInstance_install() {
 	local INSTANCE_NAME="${1}"
-	service ${INSTANCE_NAME} stop
-	update-rc.d ${INSTANCE_NAME} remove
-	rm -f /etc/init.d/${INSTANCE_NAME}
-	rm -f /etc/default/${INSTANCE_NAME}
-	userdel ${INSTANCE_NAME}
-	rm -Rf /home/${INSTANCE_NAME}
+	echo "Create Virtual Environment and install Octoprint for ${INSTANCE_NAME} ..."
+	sudo -u ${INSTANCE_NAME} sh -c 'cd /home/'${INSTANCE_NAME}' && mkdir OctoPrint && cd OctoPrint && \
+	virtualenv venv && \
+	. venv/bin/activate && \
+	pip install pip --upgrade && \
+	pip install https://get.octoprint.org/latest'
+}
+
+function OctoPrintInstance_configure() {
+	local INSTANCE_NAME="${1}"
+	INSTANCE_API_SYSTEM=$(date | md5sum | awk -F" " '{print toupper($1)}')
+	INSTANCE_API_ADMIN=$(date | md5sum | awk -F" " '{print toupper($1)}')
+	INSTANCE_SALT="QmcSp5B7fubFuyTFkBMIbIs8fkahkbRf"
+	INSTANCE_PASS="e90d9e087935fb5b0d5b0b3f66a44b0459fefe41b52e9a79c397c2ec1cffc7162a51de796ee85839990ed91e4358c358bf664e796aa9ebe878329a4f1e5022fe"
+	
+	echo "Configuring Octoprint for ${INSTANCE_NAME} ..."
+	mkdir /home/${INSTANCE_NAME}/.octoprint
+	cat <<EOF > /home/${INSTANCE_NAME}/.octoprint/config.yaml
+accessControl:
+salt: ${INSTANCE_SALT}
+api:
+key: ${INSTANCE_API_SYSTEM}
+appearance:
+color: black
+defaultLanguage: en
+name: OctoPrint Instance ${INSTANCE_NAME}
+server:
+commands:
+	serverRestartCommand: sudo service ${INSTANCE_NAME} restart
+	systemRestartCommand: sudo shutdown -r now
+	systemShutdownCommand: sudo shutdown -h now
+firstRun: false
+onlineCheck:
+	enabled: true
+pluginBlacklist:
+	enabled: true
+seenWizards:
+	corewizard: 3
+	cura: null
+EOF
+	cat <<EOF > /home/${INSTANCE_NAME}/.octoprint/users.yaml
+admin:
+active: true
+apikey: null
+apikey: ${INSTANCE_API_ADMIN}
+password: ${INSTANCE_PASS}
+roles:
+- user
+- admin
+settings: {}
+EOF
+	
+	chown -R ${INSTANCE_NAME}:${INSTANCE_NAME} /home/${INSTANCE_NAME}/.octoprint
+	chmod -R +x /home/${INSTANCE_NAME}/.octoprint
 }
 
 function OctoPrintInstance_createService() {
@@ -70,90 +118,44 @@ function OctoPrintInstance_createService() {
 	service ${INSTANCE_NAME} start
 }
 
+function OctoPrintInstance_delete() {
+	local INSTANCE_NAME="${1}"
+	service ${INSTANCE_NAME} stop
+	update-rc.d ${INSTANCE_NAME} remove
+	rm -f /etc/init.d/${INSTANCE_NAME}
+	rm -f /etc/default/${INSTANCE_NAME}
+	userdel ${INSTANCE_NAME}
+	rm -Rf /home/${INSTANCE_NAME}
+}
+
 ################################################################################
 
 if [ -n ${1} ]; then
 	INSTANCE_NUM=${1}
 fi
 
-INSTANCE_NAME="op_"${INSTANCE_NUM}
 
 if [ -z ${INSTANCE_NUM} ]; then
 	echo "Argument 1 has to be a unique User Identifier, like '001' !!!"
 	exit 1
 fi
 
+INSTANCE_NAME="op_"${INSTANCE_NUM}
+
 ################################################################################
 
 echo "Creating new OctoPrint Instance with User ${INSTANCE_NAME} ..."
-
 OctoPrintInstance_createUser ${INSTANCE_NAME}
-
-################################################################################
-
 OctoPrintInstance_installDeps
-
-echo "Create Virtual Environment and install Octoprint for ${INSTANCE_NAME} ..."
-sudo -u ${INSTANCE_NAME} sh -c 'cd /home/'${INSTANCE_NAME}' && mkdir OctoPrint && cd OctoPrint && \
-virtualenv venv && \
-. venv/bin/activate && \
-pip install pip --upgrade && \
-pip install https://get.octoprint.org/latest'
-
-INSTANCE_API_SYSTEM=$(date | md5sum | awk -F" " '{print toupper($1)}')
-INSTANCE_API_ADMIN=$(date | md5sum | awk -F" " '{print toupper($1)}')
-INSTANCE_SALT="QmcSp5B7fubFuyTFkBMIbIs8fkahkbRf"
-INSTANCE_PASS="e90d9e087935fb5b0d5b0b3f66a44b0459fefe41b52e9a79c397c2ec1cffc7162a51de796ee85839990ed91e4358c358bf664e796aa9ebe878329a4f1e5022fe"
-
-echo "Configuring Octoprint for ${INSTANCE_NAME} ..."
-mkdir /home/${INSTANCE_NAME}/.octoprint
-cat <<EOF > /home/${INSTANCE_NAME}/.octoprint/config.yaml
-accessControl:
-  salt: ${INSTANCE_SALT}
-api:
-  key: ${INSTANCE_API_SYSTEM}
-appearance:
-  color: black
-  defaultLanguage: en
-  name: OctoPrint Instance ${INSTANCE_NUM}
-server:
-  commands:
-    serverRestartCommand: sudo service ${INSTANCE_NAME} restart
-    systemRestartCommand: sudo shutdown -r now
-    systemShutdownCommand: sudo shutdown -h now
-  firstRun: false
-  onlineCheck:
-    enabled: true
-  pluginBlacklist:
-    enabled: true
-  seenWizards:
-    corewizard: 3
-    cura: null
-EOF
-cat <<EOF > /home/${INSTANCE_NAME}/.octoprint/users.yaml
-admin:
-  active: true
-  apikey: null
-  apikey: ${INSTANCE_API_ADMIN}
-  password: ${INSTANCE_PASS}
-  roles:
-  - user
-  - admin
-  settings: {}
-EOF
-
-chown -R ${INSTANCE_NAME}:${INSTANCE_NAME} /home/${INSTANCE_NAME}/.octoprint
-chmod -R +x /home/${INSTANCE_NAME}/.octoprint
-
-################################################################################
-
+OctoPrintInstance_install ${INSTANCE_NAME}
+OctoPrintInstance_configure ${INSTANCE_NAME}
 OctoPrintInstance_createService ${INSTANCE_NAME}
 
 exit 0
 
 ################################################################################
 # delete a existing OctoPrint Instance
-Delete_OctoPrintInstance ${INSTANCE_NAME}
+OctoPrintInstance_delete ${INSTANCE_NAME}
 
 ################################################################################
 
